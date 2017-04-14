dbbb1f350c6d3de5dc9c9af5ea4acea74278367d
cpasyanos <pasyanos.c@husky.neu.edu>
2017-04-14 14:32:15 -0400

    added skeletons for adapter

--- src/cs3500/music/view/AProviderView.java
+++ src/cs3500/music/view/AProviderView.java
@@ -1,7 +1,7 @@
 package cs3500.music.view;
 
-import cs3500.music.model.IPiece;
-import cs3500.music.provider.view.IView;
+// import cs3500.music.model.IPiece;
+// import cs3500.music.provider.view.IView;
 
 
 /**
@@ -12,14 +12,8 @@ public abstract class AProviderView implements cs3500.music.view.IView {
 
   /**
    * The constructor takes a provider view and assigns it to the adapter.
-   * @param view the provider view.
    */
-  public AProviderView(cs3500.music.provider.view.IView view) {
-    this.view = view;
-  }
-
-  @Override
-  public void assignPiece(IPiece piece) {
-
+  public AProviderView() {
+    this.view = null;
   }
 }
---
+++ src/cs3500/music/view/ProviderGUIView.java
@@ -0,0 +1,57 @@
+package cs3500.music.view;
+
+import cs3500.music.model.IPiece;
+import cs3500.music.model.Note;
+
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+
+/**
+ * The concrete adapter of our provider's gui view to our view interface.
+ */
+public class ProviderGUIView extends AProviderView {
+  @Override
+  public void assignPiece(IPiece piece) {
+
+  }
+
+  @Override
+  public void show(int beat) {
+
+  }
+
+  @Override
+  public void scrollForward() {
+
+  }
+
+  @Override
+  public void scrollBack() {
+
+  }
+
+  @Override
+  public void addKeyListener(KeyListener listener) {
+
+  }
+
+  @Override
+  public void addMouseListener(MouseListener listener) {
+
+  }
+
+  @Override
+  public void togglePause() {
+
+  }
+
+  @Override
+  public Note getNoteOnPiano() {
+    return null;
+  }
+
+  @Override
+  public void update() {
+
+  }
+}
